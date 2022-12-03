6cc8499246e46103cca3303229ba0047fa3b688c
Blair Frandeen <blairfrandeen@gmail.com>
2022-12-03 09:19:33 -0800

    day 3 WIP

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,5 +1,26 @@
 pub fn main(contents: String) {
-    println!("Hello AoC!")
+    println!("Hello AoC!");
+/*    contents.split('\n').map(|word| {
+        let split = split_word(&word);
+
+    }
+*/
+}
+
+fn find_common(words: (&str, &str)) -> char {
+    let w1: Vec<char> = words.0.chars().collect();
+    let w2: Vec<char> = words.1.chars().collect();
+    println!("{:?}", w1);
+    println!("{:?}", w1[0]);
+    if w1.len() != w2.len() {
+        panic!("Words not the same length!")
+    }
+    for index in [..w1.len()] {
+        if w1[index] == w2[index] {
+            return w1[index];
+        }
+    }
+    unreachable!("No match found!")
 }
 
 
@@ -9,7 +30,11 @@ fn split_word(word: &str) -> (&str, &str) {
 }
 
 fn get_priority(item: char) -> usize {
-    item as usize// - 96
+    let ord = match item.is_uppercase() {
+        true => 'A' as usize - 26,
+        false => 'a' as usize,
+    };
+    item as usize - ord + 1
 }
 
 #[cfg(test)]
@@ -17,9 +42,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_common() {
+        assert_eq!(find_common(("big", "log")), 'g');
+    }
+
+    #[test]
     fn test_priority() {
-        // assert_eq!(1, get_priority('a'));
-        // assert_eq!(16, get_priority('p'));
+        assert_eq!(1, get_priority('a'));
+        assert_eq!(16, get_priority('p'));
         assert_eq!(38, get_priority('L'));
         assert_eq!(42, get_priority('P'));
     }
