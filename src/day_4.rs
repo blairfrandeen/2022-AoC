0a8eb01438e78d8f64381a7898e2ad456d85d72b
Blair Frandeen <blairfrandeen@gmail.com>
2022-12-03 21:50:20 -0800

    day 4 complete

--- src/day_4.rs
+++ src/day_4.rs
@@ -8,6 +8,8 @@ Want to start with a parsing function that returns two tuples
 pub fn main(contents: String) {
     let part_1_ans = part_1(&contents);
     println!("Part 1: {part_1_ans}");
+    let part_2_ans = part_2(&contents);
+    println!("Part 2: {part_2_ans}");
 }
 
 fn part_1(contents: &str) -> i32 {
@@ -17,6 +19,19 @@ fn part_1(contents: &str) -> i32 {
             break;
         }
         if contains(parse_line(line)) {
+            sum += 1;
+        }
+    }
+    sum
+}
+
+fn part_2(contents: &str) -> i32 {
+    let mut sum = 0;
+    for line in contents.split('\n') {
+        if line.is_empty() {
+            break;
+        }
+        if overlaps(parse_line(line)) {
             sum += 1;
         }
     }
@@ -35,6 +50,11 @@ fn contains(assignment: Vec<i32>) -> bool {
         || (assignment[0] >= assignment[2] && assignment[1] <= assignment[3])
 }
 
+fn overlaps(assignment: Vec<i32>) -> bool {
+    (assignment[1] >= assignment[2] && assignment[3] >= assignment[0])
+        || (assignment[1] <= assignment[2] && assignment[3] <= assignment[0])
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -45,6 +65,15 @@ mod tests {
         assert_eq!(parse_line(line), vec![19, 84, 6, 17]);
     }
 
+    #[test]
+    fn test_overlap() {
+        assert!(overlaps(vec![10, 20, 12, 18]));
+        assert!(overlaps(vec![10, 20, 5, 25]));
+        assert!(overlaps(vec![10, 28, 5, 25]));
+        assert!(!overlaps(vec![10, 20, 40, 48]));
+        assert!(!overlaps(vec![10, 20, 5, 6]));
+        assert!(!overlaps(vec![5, 6, 10, 20]));
+    }
     #[test]
     fn test_contains() {
         assert!(contains(vec![10, 20, 12, 18]));
