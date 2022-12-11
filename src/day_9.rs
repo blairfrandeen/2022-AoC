c480140ee17396c286d7b44fb74e663b6b1a7f9a
Blair Frandeen <blairfrandeen@gmail.com>
2022-12-11 00:08:46 -0800

    day 9 part 1 complete

--- src/day_9.rs
+++ src/day_9.rs
@@ -1,4 +1,7 @@
 #![allow(unused)]
+use std::collections::HashSet;
+
+#[derive(PartialEq, Eq, Hash, Debug, Clone)]
 struct Position {
     x: i32,
     y: i32,
@@ -14,11 +17,41 @@ impl Position {
             _ => panic!("Wrong direction!"),
         };
     }
-    fn follow(&mut self, other: Position) {
-        if self.x - other.x > 1 {
-            self.x -= 1;
-        } else if other.x - self.x > 1 {
-            self.x += 1;
+
+    fn follow(&mut self, other: &Position) {
+        if touching(self, &other) {
+            return ();
+        }
+        // self and other are in same row or column
+        if self.x == other.x || self.y == other.y {
+            if self.x - other.x > 1 {
+                self.x -= 1; // Move left
+            } else if other.x - self.x > 1 {
+                self.x += 1; // Move right
+            } else if self.y - other.y > 1 {
+                self.y -= 1; // Move down
+            } else if other.y - self.y > 1 {
+                self.y += 1; // Move up
+            }
+        } else {
+            // move diagonal
+            if other.x > self.x && other.y > self.y {
+                // Up and Right
+                self.x += 1;
+                self.y += 1;
+            } else if other.x < self.x && other.y > self.y {
+                // Up and Left
+                self.x -= 1;
+                self.y += 1;
+            } else if other.x < self.x && other.y < self.y {
+                // Down and Left
+                self.x -= 1;
+                self.y -= 1;
+            } else if other.x > self.x && other.y < self.y {
+                // Down and Right
+                self.x += 1;
+                self.y -= 1;
+            }
         }
     }
 }
@@ -27,10 +60,29 @@ fn touching(p1: &Position, p2: &Position) -> bool {
     !((p1.x - p2.x).abs() > 1 || (p1.y - p2.y).abs() > 1)
 }
 
+fn parse_instruction(input: &str) -> (char, u32) {
+    let inst: Vec<&str> = input.trim().split(' ').collect();
+    let dir: char = inst[0].parse().unwrap();
+    let num: u32 = inst[1].parse().unwrap();
+    (dir, num)
+}
+
 pub fn main(contents: String) {
     let mut head = Position { x: 0, y: 0 };
     let mut tail = Position { x: 0, y: 0 };
-    println!("Hello AoC!")
+    let mut tail_history: HashSet<Position> = HashSet::new();
+    tail_history.insert(tail.clone());
+    for line in contents.lines() {
+        // println!("{}", &line);
+        let (dir, num) = parse_instruction(line);
+        for _ in 0..num {
+            head.lead(dir);
+            tail.follow(&head);
+            // println!("{:?} {:?}", &head, &tail);
+            tail_history.insert(tail.clone());
+        }
+    }
+    println!("Part 1: {}", tail_history.len())
 }
 
 #[cfg(test)]
@@ -38,14 +90,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse() {
+        let input = "R 4\n";
+        assert_eq!(parse_instruction(input), ('R', 4));
+        assert_eq!(parse_instruction("L 5\n"), ('L', 5));
+        assert_eq!(parse_instruction("L 15\n"), ('L', 15));
+    }
+    #[test]
+    fn test_follow() {
+        let mut p1 = Position { x: 0, y: 0 };
+        let p3 = Position { x: 0, y: 2 };
+        p1.follow(&p3);
+        assert_eq!(p1, Position { x: 0, y: 1 });
+        p1.follow(&p3); // should not move
+        assert_eq!(p1, Position { x: 0, y: 1 });
+        let p2 = Position { x: 1, y: 3 };
+        p1.follow(&p2);
+        assert_eq!(p1, Position { x: 1, y: 2 });
+    }
+    #[test]
     fn test_touch() {
         let p1 = Position { x: 0, y: 0 };
         let p2 = Position { x: 1, y: 1 };
         let p3 = Position { x: 0, y: 2 };
         let p4 = Position { x: 0, y: 0 };
+        let p5 = Position { x: 1, y: 0 };
         assert!(touching(&p1, &p4));
         assert!(touching(&p1, &p2));
         assert!(!touching(&p1, &p3));
+        assert!(touching(&p1, &p5));
+        assert!(!touching(&p5, &p3));
     }
     #[test]
     fn test_lead() {
